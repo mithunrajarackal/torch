28f166cddc3fd8f8a271f6cc3d1daa854090d987
aruntomy31 <jamestommy007@gmail.com>
2015-05-14 00:35:30 +0530

    Up and running

--- src/com/armi/torch/TorchActivity.java
+++ src/com/armi/torch/TorchActivity.java
@@ -3,17 +3,17 @@ package com.armi.torch;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class TorchActivity extends Activity {
@@ -21,6 +21,7 @@ public class TorchActivity extends Activity {
 	boolean click=false;
     Camera cam;
     Parameters p;
+    ImageView v1,v2;
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +32,21 @@ public class TorchActivity extends Activity {
 	                            WindowManager.LayoutParams.FLAG_FULLSCREEN);
 	  //  View view = this.getWindow().getDecorView();
 	   // view.setBackgroundColor(Color.WHITE);
+	    setContentView(R.layout.activity_torch);
 	    cam=Camera.open();    
 		p = cam.getParameters();
-	    ImageButton b=new ImageButton(context);
-	    b=(ImageButton)findViewById(R.id.button);
-	    
-	    
-	    
-	    //arun tomy comment
-	    float curBrightnessValue = 0;
+	    ImageButton b;
+	    b=(ImageButton)findViewById(R.id.ibutton);
+	    b.setClickable(false);
+	    v1=(ImageView)findViewById(R.id.off);
+	    v2=(ImageView)findViewById(R.id.on);
+	    v1.setVisibility(View.VISIBLE);
+	    v2.setVisibility(View.INVISIBLE);
+	   	float curBrightnessValue = 0;
 	    try {
 	    	if(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH))
 	    	{
+	    		b.setClickable(true);	
 	    		//b.setOnClickListener((OnClickListener) this);
 	    		//Toast.makeText(getApplicationContext(), "You can use LED torch :)", Toast.LENGTH_LONG).show();
 	    		 
@@ -79,14 +83,25 @@ public class TorchActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		setContentView(R.layout.activity_torch);
+		
 	}
-	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)  {
+	    if (keyCode == KeyEvent.KEYCODE_BACK ) {
+	    	cam.release();	
+	        // do something on back.
+	        //return true;
+	    }
+
+	    return super.onKeyDown(keyCode, event);
+	}
 	@SuppressWarnings("deprecation")
-	void lightTorch(View v)
+	public void lightTorch(View v)
 	 {
 		if(click==false)
 		{
+			v1.setVisibility(View.INVISIBLE);
+		    v2.setVisibility(View.VISIBLE);
 			click=true;
 			p.setFlashMode(Parameters.FLASH_MODE_TORCH);
     		cam.setParameters(p);
@@ -94,9 +109,11 @@ public class TorchActivity extends Activity {
 		}
 		else
 		{
+			v1.setVisibility(View.VISIBLE);
+		    v2.setVisibility(View.INVISIBLE);
 			click=false;
 			cam.stopPreview();
-    		cam.release();	
+    		
 		}
 	 }
 }
